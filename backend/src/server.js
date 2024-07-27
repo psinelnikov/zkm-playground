26c8c2da821a65bc7f2b6dc0855a220501f926a4
Pavel Sinelnikov <pavel@sinelnikov.xyz>
2024-07-26 21:30:12 -0400

    Fixed initial proof not being displayed

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const express = require("express");
+const generateProof = require("./generateProof");
+const cors = require("cors");
+const app = express();
+let isProcessing = false;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://playground.zkm.io",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello from prover-service!");
+});
+
+app.post("/generateProof", async (req, res) => {
+  if (isProcessing) {
+    res.send("Service is Busy");
+    return;
+  }
+  const { code, input } = req.body;
+
+  isProcessing = true;
+  await generateProof(code, input, res);
+  isProcessing = false;
+});
+
+const portHttp = 8080;
+
+const server = http.createServer(app);
+
+server.listen(portHttp, () => {
+  console.log(`Server is running on http://localhost:${portHttp}`);
+});
